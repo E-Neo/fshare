2d912a82b3ecc711473697403cd1699f649d8b13
E-Neo <e-neo@qq.com>
2020-03-15 20:08:58 +0800

    add passive mode

--- README.md
+++ README.md
@@ -4,21 +4,22 @@ A simple and stupid tool to share a file by TCP.
 
 ## Build
 
-   ```sh
-   $ cargo build --release
-   ```
+```sh
+$ cargo build --release
+```
 
 ## Example
 
 Transfer a file from QEMU guest to host under the default user-mode networking:
-1. On the host:
 
-   ```sh
-   $ fshare receive /path/to/output/file
-   ```
+```sh
+ (host) $ fshare recv /path/to/output/file
+(guest) $ fshare post 10.0.2.2:8000 /path/to/input/file
+```
 
-2. On the guest:
+Transfer a file from host to QEMU guest under the default user-mode networking:
 
-    ```sh
-    $ fshare send /path/to/input/file 10.0.2.2:8000
-    ```
+```sh
+ (host) $ fshare send /path/to/input/file
+(guest) $ fshare get 10.0.2.2:8000 /path/to/output/file
+```
--- src/main.rs
+++ src/main.rs
@@ -4,42 +4,59 @@ use std::io::{BufReader, BufWriter, Result};
 use std::net::{TcpListener, TcpStream, ToSocketAddrs};
 use std::path::Path;
 
-struct Sender;
-
-impl Sender {
-    fn new() -> Self {
-        Sender
-    }
-
-    fn send<A: ToSocketAddrs, P: AsRef<Path>>(&self, path: P, addr: A) -> Result<()> {
-        file_to_stream(path, TcpStream::connect(addr)?)?;
-        Ok(())
-    }
-}
-
-struct Receiver {
+struct Server {
     listener: TcpListener,
 }
 
-impl Receiver {
+impl Server {
     fn new<A: ToSocketAddrs>(addr: A) -> Self {
-        Receiver {
+        Server {
             listener: TcpListener::bind(addr).unwrap(),
         }
     }
 
-    fn receive<P: AsRef<Path>>(&self, path: P) -> Result<()> {
+    fn recv<P: AsRef<Path>>(&self, path: P) -> Result<u64> {
         println!("Receiver: {:?}", self.listener.local_addr().unwrap());
         match self.listener.accept() {
             Ok((stream, addr)) => {
-                println!("Sender:   {:?}", addr);
-                stream_to_file(stream, path)?;
+                println!("Client:   {:?}", addr);
+                stream_to_file(stream, path)
             }
             Err(e) => {
                 println!("Error:    {:?}", e);
+                Err(e)
             }
         }
-        Ok(())
+    }
+
+    fn send<P: AsRef<Path>>(&self, path: P) -> Result<u64> {
+        println!("Sender: {:?}", self.listener.local_addr().unwrap());
+        match self.listener.accept() {
+            Ok((stream, addr)) => {
+                println!("Client: {:?}", addr);
+                file_to_stream(path, stream)
+            }
+            Err(e) => {
+                println!("Error:  {:?}", e);
+                Err(e)
+            }
+        }
+    }
+}
+
+struct Client;
+
+impl Client {
+    fn new() -> Self {
+        Client
+    }
+
+    fn get<A: ToSocketAddrs, P: AsRef<Path>>(&self, addr: A, path: P) -> Result<u64> {
+        stream_to_file(TcpStream::connect(addr)?, path)
+    }
+
+    fn post<A: ToSocketAddrs, P: AsRef<Path>>(&self, addr: A, path: P) -> Result<u64> {
+        file_to_stream(path, TcpStream::connect(addr)?)
     }
 }
 
@@ -58,33 +75,49 @@ fn stream_to_file<P: AsRef<Path>>(stream: TcpStream, path: P) -> Result<u64> {
 }
 
 fn main() -> Result<()> {
-    let matches = App::new("Simple TCP file sender/receiver")
+    let matches = App::new("Simple TCP file-sharing tool")
         .version("0.1.0")
         .author("E-Neo <e-neo@qq.com>")
-        .about("As simple as possible")
         .subcommand(
             SubCommand::with_name("send")
+                .about("Server to send a file")
                 .arg(Arg::with_name("PATH").required(true))
-                .arg(Arg::with_name("ADDR").required(true)),
+                .arg(Arg::with_name("ADDR").default_value("0.0.0.0:8000")),
         )
         .subcommand(
-            SubCommand::with_name("receive")
+            SubCommand::with_name("recv")
+                .about("Server to receive a file")
                 .arg(Arg::with_name("PATH").required(true))
-                .arg(
-                    Arg::with_name("bind")
-                        .value_name("ADDR")
-                        .default_value("0.0.0.0:8000"),
-                ),
+                .arg(Arg::with_name("ADDR").default_value("0.0.0.0:8000")),
+        )
+        .subcommand(
+            SubCommand::with_name("get")
+                .about("Client to get a file")
+                .arg(Arg::with_name("ADDR").required(true))
+                .arg(Arg::with_name("PATH").required(true)),
+        )
+        .subcommand(
+            SubCommand::with_name("post")
+                .about("Client to post a file")
+                .arg(Arg::with_name("ADDR").required(true))
+                .arg(Arg::with_name("PATH").required(true)),
         )
         .get_matches();
+
     if let Some(matches) = matches.subcommand_matches("send") {
-        Sender::new().send(
-            matches.value_of("PATH").unwrap(),
+        Server::new(matches.value_of("ADDR").unwrap()).send(matches.value_of("PATH").unwrap())?;
+    } else if let Some(matches) = matches.subcommand_matches("recv") {
+        Server::new(matches.value_of("ADDR").unwrap()).recv(matches.value_of("PATH").unwrap())?;
+    } else if let Some(matches) = matches.subcommand_matches("get") {
+        Client::new().get(
             matches.value_of("ADDR").unwrap(),
+            matches.value_of("PATH").unwrap(),
         )?;
-    } else if let Some(matches) = matches.subcommand_matches("receive") {
-        Receiver::new(matches.value_of("bind").unwrap())
-            .receive(matches.value_of("PATH").unwrap())?;
+    } else if let Some(matches) = matches.subcommand_matches("post") {
+        Client::new().post(
+            matches.value_of("ADDR").unwrap(),
+            matches.value_of("PATH").unwrap(),
+        )?;
     } else {
         println!("{}", matches.usage());
     }
